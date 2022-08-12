5261a8bf22971ed6052fe706f2c29853002a8ec3
Henrique_Veras <theveras1@gmail.com>
2022-08-12 04:36:56 -0300

    teste config

--- config/config.js
+++ config/config.js
@@ -6,14 +6,26 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.DB,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.DB,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: process.env.MYSQL_USER,
